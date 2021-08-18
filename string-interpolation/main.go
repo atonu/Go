8194a9836b2ff5fc88c21959ed4032c3787d412b
ahmed zahin <atonu.zahin@gmail.com>
2021-08-19 03:30:45 +0600

    optimized string ops

--- string-interpolation/main.go
+++ string-interpolation/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,7 +13,10 @@ var read *bufio.Reader
 func main() {
 	read = bufio.NewReader(os.Stdin)
 	userName := ReadString("What's your name?")
-	fmt.Println("Your name is", userName)
+	age := ReadInt("How old r u?")
+	// fmt.Println("Your name is", userName, "and you are", age, "years old")
+	// fmt.Println(fmt.Sprintf("Your name is %s. You are %d years old.", userName, age))
+	fmt.Printf("Your name is %s. You are %d years old.\n", userName, age)
 }
 
 func promt() {
@@ -20,11 +24,31 @@ func promt() {
 }
 
 func ReadString(s string) string {
+	for {
+		fmt.Println(s)
+		promt()
+		userName, _ := read.ReadString('\n')
+		userName = strings.Replace(userName, "\r\n", "", -1)
+		userName = strings.Replace(userName, "\n", "", -1)
+		if userName == "" {
+			fmt.Println("fam pls")
+		} else {
+			return userName
+		}
+	}
+}
+
+func ReadInt(s string) int {
 	fmt.Println(s)
 	promt()
 	userName, _ := read.ReadString('\n')
 	userName = strings.Replace(userName, "\r\n", "", -1)
 	userName = strings.Replace(userName, "\n", "", -1)
 
-	return userName
+	num, err := strconv.Atoi(userName)
+	if err != nil {
+		fmt.Println("Enter number pls")
+		return ReadInt(s)
+	}
+	return num
 }
